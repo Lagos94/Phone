3a7b1a60f89ba2148b1be2d5d25ed79548b580b4
Lawliet94 <31496705+Lawliet94@users.noreply.github.com>
2017-10-14 21:02:54 -0400

    Add files via upload

---
+++ Phone/src/Phone.java
@@ -0,0 +1,46 @@
+/*By Juan Lagos
+* CSC 201
+* Professor Tanes Kanchanawanchai
+* Assignment 4 Chapter 4
+* Computer Configuration
+* Due Date: 10/14/17*/
+
+import java.util.Scanner;
+
+public class Phone {
+    public static void main(String[] args) {
+        Scanner uInput = new Scanner(System.in);
+        while (uInput.hasNext()) {
+            System.out.println("Enter a letter: ");
+            String letter = uInput.nextLine();
+            char character = letter.charAt(0);
+            character = Character.toLowerCase(character);
+
+            int number = 0;
+            if (Character.isLetter(character)) {
+                if (character >= 'w')
+                    number = 9;
+                else if (character >= 't')
+                    number = 8;
+                else if (character >= 'p')
+                    number = 7;
+                else if (character >= 'm')
+                    number = 6;
+                else if (character >= 'j')
+                    number = 5;
+                else if (character >= 'g')
+                    number = 4;
+                else if (character >= 'd')
+                    number = 3;
+                else if (character >= 'a')
+                    number = 2;
+                System.out.println("The corresponding number is " + number + ".");
+                // The numbers 1 and 0 are not included since the phone key pads described
+                // by the exercise serve for purposes beyond the scope of this program.
+            } else {
+                System.out.println(character + " is an invalid input!");
+            }
+            System.out.println("Enter a letter: ");
+        }
+    }
+}
